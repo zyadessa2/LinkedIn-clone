eab809cf57290bf5aa2d9cf4b88ea80d362576e6
ziad <helmyzez@gmail.com>
2024-03-08 13:43:53 +0200

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import { BrowserRouter as Router , Routes , Route  } from 'react-router-dom'
+import Login from './Components/Login'
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <>
+  <div className="app">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </Router>
+  </div>
+  </>
 }
 
 export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+  return <>
+  <Container>
+    <Nav>
+        <a href="/index.html">
+            <img src="" alt="" />
+        </a>
+    </Nav>
+  </Container>
+  </>
+}
+
+const Container = styled.div``;
+const Nav = styled.nav``;
+export default Login
